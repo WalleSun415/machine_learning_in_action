f2e1db00b423cad8f3af82b18426638c8e8f39a3
WalleSun415 <672415342@qq.com>
2017-03-15 16:29:12 +0800

    add bestFeature function

--- DecisionTree/trees.py
+++ DecisionTree/trees.py
@@ -1,4 +1,18 @@
+# coding=utf-8
 from math import log
+import operator
+
+
+def createDataSet():
+    dataSet = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [0, 1, 'no']
+    ]
+    label = ['no surfacing', 'flippers']
+    return dataSet, label
 
 
 def calcShannonEnt(dataSet):
@@ -6,7 +20,7 @@ def calcShannonEnt(dataSet):
     labelCounts = {}
     for featVec in dataSet:
         currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys()
+        if currentLabel not in labelCounts.keys():
             labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
     shannonEnt = 0.0
@@ -14,3 +28,49 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key]) / numEntries
         shannonEnt -= prob * log(prob, 2)
     return shannonEnt
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]  # 抽取要求数据外的前部剩余元素
+            reducedFeatVec.extend(featVec[axis+1:])  # 抽取要求数据外的后部剩余元素
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)  # 将featList转换成set数据结构
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+if __name__ == '__main__':
+    myDat, labels = createDataSet()
+    chooseBestFeatureToSplit(myDat)
