eab966ef57de958685e6cf8865ed1c6deac24ac2
WalleSun415 <672415342@qq.com>
2017-03-15 19:48:51 +0800

    add createTree function

--- DecisionTree/trees.py
+++ DecisionTree/trees.py
@@ -57,7 +57,6 @@ def chooseBestFeatureToSplit(dataSet):
         if infoGain > bestInfoGain:
             bestInfoGain = infoGain
             bestFeature = i
-
     return bestFeature
 
 
@@ -71,6 +70,27 @@ def majorityCnt(classList):
     return sortedClassCount[0][0]
 
 
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if dataSet[0] == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]  # 复制当前label列表，避免每次调用时改变原label列表
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
 if __name__ == '__main__':
     myDat, labels = createDataSet()
     chooseBestFeatureToSplit(myDat)
+    myTree = createTree(myDat, labels)
+    print(myTree)
+
