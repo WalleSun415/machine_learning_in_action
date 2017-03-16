6a0716c8caaa9b25642d549382d3d40acf74297c
WalleSun415 <672415342@qq.com>
2017-03-16 17:22:02 +0800

    add train&test function

--- naiveBayes/bayes.py
+++ naiveBayes/bayes.py
@@ -1,3 +1,7 @@
+# coding=utf-8
+from numpy import *
+
+
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -24,3 +28,62 @@ def setOfWord2Vec(vocabList, inputSet):
         else:
             print("the word: %s not in my Vocabulary!" % word)
     return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)  # 侮辱性类别文档的概率
+    '''不采用拉普拉斯平滑
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    '''
+    '''采用拉普拉斯平滑，类数量K为2，则各个条件概率分母+2，分子+1'''
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]  # List中加号为扩展，array中加号为对应元素相加
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    '''可能发生下溢出
+    p1Vect = p1Num/p1Denom  # 侮辱性类别词汇的条件概率数组
+    p0Vect = p0Num/p0Denom  # 无侮辱性类别词汇的条件概率数组
+    '''
+    '''采用对乘积取自然对数，防止发生下溢出'''
+    p1Vect = log(p1Num/p1Denom)
+    p0Vect = log(p0Num/p0Denom)
+
+    return p0Vect, p1Vect, pAbusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWord2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWord2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWord2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
