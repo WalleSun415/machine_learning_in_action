714f2fd7bed1149d00e401ec235ff08d670603cb
WalleSun415 <672415342@qq.com>
2017-03-17 10:56:34 +0800

    add gradAscent function

---
+++ LogisticRegression/logRegres.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+from numpy import *
+
+
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+def sigmoid(inx):
+    return 1.0 / (1 + exp(-inx))
+
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(dataMatrix)
+    maxCycles = 500
+    alpha = 0.001
+    weights = ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix * weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return weights
+
+
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
