b16c0a8e82181d43260759a65ab8ba5173369145
WalleSun415 <672415342@qq.com>
2017-03-17 18:07:07 +0800

    add colicTest function

--- LogisticRegression/logRegres.py
+++ LogisticRegression/logRegres.py
@@ -80,8 +80,52 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     return weights
 
 
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1
+    else:
+        return 0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 500)
+    errorCount = 0; numTestVes = 0.0
+    for line in frTest.readlines():
+        numTestVes += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVes)
+    print("the error rate of this test is: %f" % errorRate)
+    return errorRate
+
+
+def multiTest():
+    numTests = 10; errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests)))
+
+
 if __name__ == '__main__':
+    '''
     dataArr, labelMat = loadDataSet()
     weights = stocGradAscent1(array(dataArr), labelMat)
     print(weights)
-    # plotBestFit(weights)
+    plotBestFit(weights)
+    '''
+    multiTest()
