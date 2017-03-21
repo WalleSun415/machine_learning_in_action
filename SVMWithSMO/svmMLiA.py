4d5033cccfe9a732f69a0596f8ff970f612854b5
WalleSun415 <672415342@qq.com>
2017-03-21 21:27:07 +0800

    完整SMO算法的内循环

--- SVMWithSMO/svmMLiA.py
+++ SVMWithSMO/svmMLiA.py
@@ -77,10 +77,88 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     return b, alphas
 
 
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m, 2)))
+
+
+def calcEk(oS, k):
+    fXk = float(multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k, :].T)) + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+
+def selectJ(i, oS, Ei):
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    oS.eCache[i] = [1, Ei]
+    validEcacheList = nonzero(oS.eCache[:, 0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i: continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)  # 选择使|E1-E2|最大的α2
+            if deltaE > maxDeltaE:
+                maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+        return j, Ej
+
+
+def updateEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1, Ek]
+
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i] * Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i] * Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy()
+        if oS.labelMat[i] != oS.labelMat[j]:
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L == H: print("L==H");return 0
+        eta = 2.0 * oS.X[i, :] * oS.X[j, :].T - oS.X[i, :] * oS.X[i, :].T - oS.X[j, :] * oS.X[j, :].T
+        if eta >= 0: print("eta>=0"); return 0
+        oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if abs(oS.alphas[j] - alphaJold) < 0.00001:  # 判断α2是否有足够大的变化
+            print("j not moving enough"); return 0
+        oS.alphas[i] += oS.labelMat[i] * oS.labelMat[j] * (alphaJold - oS.alphas[j])
+        updateEk(oS, i)
+        b1 = oS.b - Ei - oS.labelMat[i] * oS.X[i, :] * oS.X[i, :].T * (oS.alphas[i] - alphaIold) - \
+             oS.labelMat[j] * oS.X[j, :] * oS.X[i, :].T * (oS.alphas[j] - alphaJold)
+        b2 = oS.b - Ej - oS.labelMat[i] * oS.X[i, :] * oS.X[j, :].T * (oS.alphas[i] - alphaIold) - \
+             oS.labelMat[j] * oS.X[j, :] * oS.X[j, :].T * (oS.alphas[j] - alphaJold)
+        if (oS.alphas[i] > 0) and (oS.alphas[i] < oS.C): oS.b = b1
+        elif (oS.alphas[j] > 0) and (oS.alphas[j] < oS.C): oS.b = b2
+        else: oS.b = (b1 + b2) / 2.0
+        return 1
+    else:
+        return 0
+
+
+
+
+
 if __name__ == '__main__':
     dataArr, labelArr = loadDataSet('testSet.txt')
-    print(dataArr)
-    print(labelArr)
-    b, alphas = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
-    print(b)
-    print(alphas[alphas > 0])
+    # print(dataArr)
+    # print(labelArr)
+    # b, alphas = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+    b, alphas = smoP(dataArr, labelArr, 0.6, 0.001, 40)
+    # print(b)
+    # print(alphas[alphas > 0])
