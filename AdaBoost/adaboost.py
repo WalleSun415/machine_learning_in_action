d1aa416110bdbf5354cea85c9406c957f5290346
WalleSun415 <672415342@qq.com>
2017-03-23 22:24:50 +0800

    add classify function

--- AdaBoost/adaboost.py
+++ AdaBoost/adaboost.py
@@ -37,7 +37,15 @@ def adaBoostTrainDS(dataArr, classLabels, numIt=40):
     return weakClassArr
 
 
-
+def adaClassify(dataToClass, classifierArr):
+    dataMatrix = mat(dataToClass)
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], classifierArr[i]['thresh'], classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+        print(aggClassEst)
+    return sign(aggClassEst)
 
 
 if __name__ == '__main__':
