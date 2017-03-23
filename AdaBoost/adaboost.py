975df95524750f3c34ea13fa0d8a65012161c3d5
WalleSun415 <672415342@qq.com>
2017-03-23 15:47:07 +0800

    添加数据集

---
+++ AdaBoost/adaboost.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from numpy import *
+
+
+def loadSimpData():
+    datMat = matrix([[1.0, 2.1],
+                     [2.0, 1.1],
+                     [1.3, 1.0],
+                     [1.0, 1.0],
+                     [2.0, 1.0]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+
+
+
+
+
