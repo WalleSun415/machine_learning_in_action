2a1da2ff197dc4ce2fb3a5e79307f677306a594e
WalleSun415 <672415342@qq.com>
2017-03-27 11:33:39 +0800

    add LocallyWeightedLinearRegression function

--- linearRegression/regression.py
+++ linearRegression/regression.py
@@ -25,16 +25,47 @@ def standRegres(xArr, yArr):
     return ws
 
 
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye(m))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = exp(diffMat * diffMat.T / (-2 * k**2))
+    xTx = xMat.T * weights * xMat
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cnanot do inverse")
+        return
+    ws = xTx.I * xMat.T * weights * yMat
+    return testPoint * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
+def rssEror(yArr, yHatArr):
+    return ((yArr-yHatArr) ** 2).sum()
+
 if __name__ == '__main__':
-    xArr, yArr = loadDataSet('ex0.txt')
-    ws = standRegres(xArr, yArr)
-    xMat = mat(xArr); yMat = mat(yArr)
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
-    xCopy = xMat.copy()
-    xCopy.sort(0)
-    yHat = xCopy * ws
-    ax.plot(xCopy[:, 1], yHat)
+    # xArr, yArr = loadDataSet('ex0.txt')
+    # yHat = lwlrTest(xArr, xArr, yArr, 0.03)
+    # ws = standRegres(xArr, yArr)
+    # xMat = mat(xArr); yMat = mat(yArr)
+    # srtInd = xMat[:, 1].argsort(0)
+    # xSort = xMat[srtInd][:, 0, :]
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.plot(xSort[:, 1], yHat[srtInd])
+    # ax.scatter(xMat[:, 1].flatten().A[0], yMat.T.flatten().A[0], s=2, c='red')
+    # xCopy = xMat.copy()
+    # ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+    # xCopy.sort(0)
+    # yHat = xCopy * ws
+    # ax.plot(xCopy[:, 1], yHat)
     plt.show()
