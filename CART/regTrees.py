c797adcd4c272b6675bf7652a98df57b359fba51
WalleSun415 <672415342@qq.com>
2017-03-28 15:15:39 +0800

    add createTree function

---
+++ CART/regTrees.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+import numpy as np
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float, curLine)  ## 将每行内容映射为浮点数
+        dataMat.append(fltLine)
+    return dataMat
+
+
+def binSplitDataSet(dataSet, feature, value):  # 将数据集根据按照特征索引和特征值切分为两个子集
+    mat0 = dataSet[np.nonzero(dataSet[:, feature] > value)[0], :][0]
+    mat1 = dataSet[np.nonzero(dataSet[:, feature] <= value)[0], :][0]
+    return mat0, mat1
+
+
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):  # 递归构建回归树
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
+    if feat is None:  # 满足停止条件时，返回叶节点值
+        return val
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = val
+    lSet, rSet = binSplitDataSet(dataSet, feat, val)
+    retTree['left'] = createTree(lSet, leafType, errType, ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops)
+    return retTree
+
+
+def regLeaf(dataSet):
+    return np.mean(dataSet[:, -1])
+
+
+def regErr(dataSet):  # impurity(不纯度)估算函数
+    return np.var(dataSet[:, -1]) * np.shape(dataSet)[0]
+
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    tolS = ops[0]  # 切分后所能容忍的误差值
+    tolN = ops[1]  # 切分的最少样本数
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:  # 如果切分后只有一个数据则直接返回
+        return None, leafType(dataSet)
+    m, n = np.shape(dataSet)
+    S = errType(dataSet)
+    bestS = np.inf; bestIndex = 0; bestValue = 0
+    for featIndex in range(n - 1):
+        for splitVal in set(dataSet[:, featIndex]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (np.shape(mat0)[0] < tolN) or (np.shape(mat1) < tolN): continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    if (np.shape(mat0)[0] < tolN) or (np.shape(mat1)[0] < tolN):
+        return None, leafType(dataSet)
+    return bestIndex, bestValue
