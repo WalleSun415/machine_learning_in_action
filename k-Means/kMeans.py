1dafdf28ff229f6df61d2b9493722b29c506df3f
WalleSun415 <672415342@qq.com>
2017-03-29 22:07:23 +0800

    add bisectingKmeans algorithm

--- k-Means/kMeans.py
+++ k-Means/kMeans.py
@@ -51,6 +51,37 @@ def kMeans(dataSet, k, disMeas=distEclud, createCent=randCent):
     return centroids, clusterAssment
 
 
+# 二分K-均值算法(bisecting K-means)
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroid0 = np.mean(dataSet, axis=0).tolist()[0]
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j, 1] = distMeas(dataSet[j, :], np.mat(centroid0))
+    while len(centList) < k:
+        lowestSSe = np.inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[np.nonzero(clusterAssment[:, 0].A == i)[0], :]
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = np.sum(splitClustAss[:, 1])
+            sseNotSplit = np.sum(clusterAssment[np.nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            print("sseSplit and notSplit: ", sseSplit, sseNotSplit)
+            if (sseSplit + sseNotSplit) < lowestSSe:
+                bestCentoSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSe = sseSplit + sseNotSplit
+        bestClustAss[np.nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)  # 重新修改簇编号以划分簇
+        bestClustAss[np.nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentoSplit  # 重新修改簇编号以划分簇
+        print("the bestCentToSplit is: ", bestCentoSplit)
+        print ("the len of bestClustAss is: ", len(bestClustAss))
+        centList[bestCentoSplit] = bestNewCents[0, :]
+        centList.append(bestNewCents[1, :])
+        clusterAssment[np.nonzero(clusterAssment[:, 0].A == bestCentoSplit)[0], :] = bestClustAss
+    return centList, clusterAssment
+
+
 if __name__ == '__main__':
     # datMat = np.mat(loadDataSet('testSet.txt'))
     # print(min(datMat[:, 0]), max(datMat[:, 0]))
@@ -58,6 +89,10 @@ if __name__ == '__main__':
     # print(randCent(datMat, 2))
     # print(distEclud(datMat[0], datMat[1]))
 
-    datMat = np.mat(loadDataSet('testSet.txt'))
-    myCentroids, clusterAssing = kMeans(datMat, 4)
-    print(myCentroids, clusterAssing)
+    # datMat = np.mat(loadDataSet('testSet.txt'))
+    # myCentroids, clusterAssing = kMeans(datMat, 4)
+    # print(myCentroids, clusterAssing)
+
+    datMat3 = np.mat(loadDataSet('testSet2.txt'))
+    centList, myNewAssments = biKmeans(datMat3, 3)
+    print(centList, myNewAssments)
