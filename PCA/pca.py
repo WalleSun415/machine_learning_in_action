43459c74d290dfd98d5ed9d9412ed0cba3697a4b
WalleSun415 <672415342@qq.com>
2017-03-30 16:01:34 +0800

    补充缺失数据&根据方差占比挑选主成分(特征值)

--- PCA/pca.py
+++ PCA/pca.py
@@ -22,13 +22,41 @@ def pca(dataMat, topNfeat=9999999):
     return lowDDataMat, reconMat
 
 
+def replaceNanWithMean():
+    datMat = loadDataSet('secom.data', ' ')
+    numFeat = np.shape(datMat)[1]
+    for i in range(numFeat):  # 将特征中的缺失值置为该特征的平均值
+        meanVal = np.mean(datMat[np.nonzero(~np.isnan(datMat[:, i].A))[0], i])  # isnan()表示取非数字值列表
+        datMat[np.nonzero(np.isnan(datMat[:, i].A))[0], i] = meanVal
+    return datMat
+
+
 if __name__ == '__main__':
-    dataMat = loadDataSet('testSet.txt')
-    lowDMat, reconMat = pca(dataMat, 1)
-    np.shape(lowDMat)
+    # dataMat = loadDataSet('testSet.txt')
+    # lowDMat, reconMat = pca(dataMat, 1)
+    # np.shape(lowDMat)
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker='^', s=90)
+    # ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s=90)
+    # plt.show()
+
+    dataMat = replaceNanWithMean()
+    meanVals = np.mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = np.cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = np.linalg.eig(np.mat(covMat))
+    print(eigVals, np.shape(eigVals))
+
+    eigValInd = np.argsort(eigVals)
+    eigValInd = eigValInd[:: -1]
+    sortedEigVals = eigVals[eigValInd]
+    total = sum(sortedEigVals)
+    varPercentage = sortedEigVals / total * 100
+
     import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker='^', s=90)
-    ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s=90)
+    ax.plot(range(0, 21), varPercentage[: 21], marker='^')
     plt.show()
