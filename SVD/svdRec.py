0c8df8c41cdd58b1a8dedb141fe89efcd72dbcfe
WalleSun415 <672415342@qq.com>
2017-03-30 23:21:38 +0800

    add recommand system

--- SVD/svdRec.py
+++ SVD/svdRec.py
@@ -1,18 +1,86 @@
 # coding=utf-8
 import numpy as np
+from numpy import linalg as la
 
 
 def loadExData():
-    return[[1, 1, 1, 0, 0],
-           [2, 2, 2, 0, 0],
-           [1, 1, 1, 0, 0],
-           [5, 5, 5, 0, 0],
-           [1, 1, 0, 2, 2],
+    return[[0, 0, 0, 2, 2],
            [0, 0, 0, 3, 3],
-           [0, 0, 0, 1, 1]]
+           [0, 0, 0, 1, 1],
+           [1, 1, 1, 0, 0],
+           [2, 2, 2, 0, 0],
+           [5, 5, 5, 0, 0],
+           [1, 1, 1, 0, 0]]
+
+
+def loadExData2():
+    return[[0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5],
+           [0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 3],
+           [0, 0, 0, 0, 4, 0, 0, 1, 0, 4, 0],
+           [3, 3, 4, 0, 0, 0, 0, 2, 2, 0, 0],
+           [5, 4, 5, 0, 0, 0, 0, 5, 5, 0, 0],
+           [0, 0, 0, 0, 5, 0, 1, 0, 0, 5, 0],
+           [4, 3, 4, 0, 0, 0, 0, 5, 5, 0, 1],
+           [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4],
+           [0, 0, 0, 2, 0, 2, 5, 0, 0, 1, 2],
+           [0, 0, 0, 0, 5, 0, 0, 0, 0, 4, 0],
+           [1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0]]
+
+
+def eulidSim(inA, inB):
+    return 1.0 / (1.0 + la.norm(inA - inB))
+
+
+def pearsSim(inA, inB):
+    if len(inA) < 3:
+        return 1.0
+
+
+def cosSim(inA, inB):
+    num = float(inA.T * inB)
+    denom = la.norm(inA) * la.norm(inB)
+    return 0.5+0.5 * (num / denom)
+
+
+def standEst(dataMat, user, simMeas, item):
+    n = np.shape(dataMat)[1]
+    simTotal = 0.0; ratSimTotal = 0.0
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0:
+            continue
+        overLap = np.nonzero(np.logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
+        if len(overLap) == 0:
+            similarity = 0
+        else:
+            similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
+        print("the %d and %d similarity is: %f" % (item, j, similarity))
+        simTotal += similarity  # 用户user栏中物品编号item与其他非0编号的物品相似度的累加
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    else:
+        return ratSimTotal / simTotal
+
+
+def recommand(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
+    unratedItems = np.nonzero(dataMat[user, :].A == 0)[1]
+    if len(unratedItems) == 0:
+        return "you rated everything"
+    itemScores = []
+    for item in unratedItems:
+        estimateScore = estMethod(dataMat, user, simMeas, item)
+        itemScores.append((item, estimateScore))
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[: N]
 
 
 if __name__ == '__main__':
-    Data = loadExData()
-    U, Sigma, VT = np.linalg.svd(Data)
-    print(Sigma)
+    # Data = loadExData()
+    # U, Sigma, VT = np.linalg.svd(Data)
+    # print(Sigma)
+
+    myMat = np.mat(loadExData())
+    myMat[0, 1] = myMat[0, 0] = myMat[1, 0] = myMat[2, 0] = 4
+    myMat[3, 3] = 2
+    print(myMat)
+    print(recommand(myMat, 2))
